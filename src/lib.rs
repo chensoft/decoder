5454055fc8b7f8ab067646b395be3185a478b8d1
Jian Chen <admin@chensoft.com>
2024-01-20 13:23:58 +0800

    proj: init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! # Decoder
+//!
+//! This library provides fast and efficient ways to decode various types from `Vec<u8>`.
+//! It avoids unnecessary allocations and copies to achieve high performance.
+//!
+//! ## How to Use
+//!
+//! Just run `cargo add decoder` to add this crate to your project, and then include the
+//! relevant trait in your code like this: `use decoder::number::Decode;`
+//!
+//! ## Example
+//!
+//! Check the relevant modules.
